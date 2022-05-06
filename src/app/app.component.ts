a8cc4fea77b94d3f9b2dd4697e03d00220e9c44d
AdrianaGabrielyan <adrin.gabrielyan@gmail.com>
2022-05-06 15:52:26 +0400

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,18 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // changeDetection:ChangeDetectionStrategy.OnPush
 
 })
 export class AppComponent implements OnInit {
 
   interval = interval(500);
 
-  constructor(private changeDetector: ChangeDetectorRef) {}
-
   ngOnInit(): void {
-    // this.interval.subscribe(val => {
-    //   this.print = val;
-    //   // this.changeDetector.markForCheck();
-    // });
+
   }
-
-
-
 
 }
